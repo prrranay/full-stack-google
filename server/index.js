2e95cff76e75067e04cd56c1986ce4f055180961
Pranay Kumar <122789579+prrranay@users.noreply.github.com>
2026-04-01 20:47:35 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,15 +4,14 @@ const port = 3000;
 
 const app=express();
 app.use(express.json());
-app.use(cors([
-    {
-        origin:"*",
-        credentials:true
-    }
-]));
+app.use(cors({
+    origin: "*"
+}));
 
 app.get("/api/message",(req,res)=>{
     res.send({data:"hello from backend"})
 })
 
-app.listen(port,()=> console.log(`you app is runnig at http://localhost:${port}`))
+app.listen(port, "0.0.0.0", () => 
+  console.log(`your app is running on port ${port}`)
+);

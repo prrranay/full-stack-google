948f08b2bbab4b5e44f118f233eedbe867629e09
Pranay <pranay.kumar@mathionix.com>
2026-04-03 20:53:39 +0530

    chore: implement environment variable support and configure dynamic API endpoints

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const express= require("express");
 const cors = require("cors");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const app=express();
 app.use(express.json());
 app.use(cors({
-    origin: "*"
+    origin: process.env.ALLOWED_ORIGIN || "*"
 }));
 
 app.get("/api/message",(req,res)=>{
